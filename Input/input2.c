3ba49455b0d81c8fe4ec40e0ca26897a1ebe7d28
CSE <gurupradeept@gmail.com>
2017-01-13 15:21:53 +0530

    input2 file and it's output added

---
+++ Input/input2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	// Single Line Comment
+	int var;
+	for(var = 10; var <= 100; var ++)
+	{
+		int temp = 0;
+		while(temp < var)
+			temp ++ ;
+		
+		/* Multi-line
+		Comment */
+
+		do
+		{
+			temp += 10 * var;
+			if(temp > 100)
+				printf("%d", temp);
+			else
+				printf("%d", var); 
+		} while(temp < 10000);
+	}
+	return 0;
+}
